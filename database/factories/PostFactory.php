2dd338aa9ef6ceb09086e126b1fe908c7a52d9d2
Huang Jia Yi <3a932117@gm.student.ncut.edu.tw>
2022-11-22 21:14:23 +0800

    lara_0901: 單元準備

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraph,
+            'is_feature' => (bool) rand(0, 1), //表示這個欄位隨機指定0 or 1 ，Factroy是工廠，用來假造資料
+        ];
+    }
+}
